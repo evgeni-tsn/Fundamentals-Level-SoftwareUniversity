7562f6c81fe41df58f3e87bb527d834a14402d50
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2015-10-13 17:50:33 +0300

    Added Java-Fundamentals Introduction to Java Homework

---
+++ Java-Fundamentals/01.Introduction-to-Java/AssignVariables.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class AssignVariables {
+
+    //Find suitable types for variables. You are given the following types:
+    // byte, short, int, long, char, boolean, float, double, and String.
+    // Assign the following values to them false, “Palo Alto, CA”, 32767,
+    // 2000000000, 0.1234567891011, 0.5f, 919827112351L, 127, ‘c’.
+    // Try to assign 32768 to short and see what happens.
+
+    //TO RUN CLICK RIGHT BUTTON AND THEN RUN MAIN;
+    //TO RUN CLICK RIGHT BUTTON AND THEN RUN MAIN;
+    //TO RUN CLICK RIGHT BUTTON AND THEN RUN MAIN;
+
+    public static void main(String[] args) {
+        byte first = 127;
+        short second = 32767;
+        int third = 2000000000;
+        long fourth = 919827112351L;
+        char fifth = 'c';
+        boolean sixth = false;
+        float seventh = 0.5f;
+        double eighth = 0.1234567891011;
+        String ninth = "Palo Alto, CA";
+    }
+}
