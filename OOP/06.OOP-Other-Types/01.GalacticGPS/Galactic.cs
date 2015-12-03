0bbca0b64bd854596afa00512b1c1a52d23c805b
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2015-12-03 16:05:52 +0200

    Added Other Type OOP Homework

---
+++ OOP/06.OOP-Other-Types/01.GalacticGPS/Galactic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.GalacticGPS
+{
+
+    /*Create a struct Location that holds fields of type double latitude and longitude of a given location. Create an enumeration Planet that holds the following constants: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune.*/ 
+
+    class Galactic
+    {
+        static void Main()
+        {
+            try
+            {
+                var planet = new Location(18.037986, 14.870097, Planet.Earth);
+                Console.WriteLine(planet);
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            try
+            {
+                var otherplanet = new Location(140.200616, 87.201879, Planet.Mars);
+                Console.WriteLine(otherplanet);
+            }
+            catch (Exception a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ OOP/06.OOP-Other-Types/03.GenericList/IGenericList.cs
@@ -0,0 +1,27 @@
+﻿namespace _03.GenericList
+{
+    public interface IGenericList<T>
+    {
+        int Capacity { get; }
+
+        int Size { get; }
+
+        T this[int index] { get; set; }
+
+        void Add(T item);
+
+        void Insert(int index, T item);
+
+        void RemoveAt(int index);
+
+        void AddRange(params T[] items);
+
+        void Clear();
+
+        int GetIndex(T item);
+
+        bool Contains(T item);
+
+        string Version();
+    }
+}
