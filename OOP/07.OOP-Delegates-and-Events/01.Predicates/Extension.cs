4fb07902fcd5ff004ddbda0ca2ad76e744dc2069
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2015-12-10 21:24:56 +0200

    OOP Delegates-and-Events homework added

---
+++ OOP/07.OOP-Delegates-and-Events/01.Predicates/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Predicates
+{
+    public static class Extension
+    {
+        public static T MyFirstOrDefault<T>(this IEnumerable<T> collection, Predicate<T> condition)
+        {
+            foreach (var element in collection.Where(element => condition(element)))
+            {
+                return element;
+            }
+
+            return default(T);
+        }
+    }
+}
