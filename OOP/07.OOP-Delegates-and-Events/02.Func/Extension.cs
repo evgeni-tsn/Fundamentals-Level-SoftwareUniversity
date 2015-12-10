da3177c33d7f734cafa942c3df428d41643142f5
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2015-12-10 21:23:32 +0200

    Added Other-Types OOP Homework

---
+++ OOP/07.OOP-Delegates-and-Events/02.Func/Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Func
+{
+    public static class Extension
+    {
+        public static IEnumerable<T> MyTakeWhile<T>(
+        this IEnumerable<T> collection,Func<T, bool> predicate)
+        {
+            return Enumerable.TakeWhile(collection, element => predicate(element)).ToList();
+        }
+    }
+}
