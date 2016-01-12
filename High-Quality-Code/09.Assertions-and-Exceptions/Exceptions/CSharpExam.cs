3738812bd7d497477d21704dbab3decbf145d8cc
evgeni.tsv.nikolov@gmail.com <evgeni.tsv.nikolov@gmail.com>
2016-01-12 13:19:35 +0200

    Added Assertions and Exceptions and Code Tuning and Optimization Homework

---
+++ High-Quality-Code/09.Assertions-and-Exceptions/Exceptions/CSharpExam.cs
@@ -0,0 +1,29 @@
+﻿namespace Exceptions_Homework
+{
+    using System;
+
+    public class CSharpExam : Exam
+    {
+        public CSharpExam(int score)
+        {
+            if (score < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(score), "Score cannot be negative!");
+            }
+
+            this.Score = score;
+        }
+
+        public int Score { get; }
+
+        public override ExamResult Check()
+        {
+            if ((this.Score < 0) || (this.Score > 100))
+            {
+                throw new InvalidOperationException("Score cannot be negative or greater than 100!");
+            }
+
+            return new ExamResult(this.Score, 0, 100, "Exam results calculated by score.");
+        }
+    }
+}
