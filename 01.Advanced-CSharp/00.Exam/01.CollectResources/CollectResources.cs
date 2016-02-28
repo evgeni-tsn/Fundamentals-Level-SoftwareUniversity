f3c5d09bcbfc5101a85e04b033770340d8f2c71b
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-02-29 00:04:41 +0200

    Advanced CSharp Retake Exam

---
+++ 01.Advanced-CSharp/00.Exam/01.CollectResources/CollectResources.cs
@@ -0,0 +1,89 @@
+﻿namespace Problem1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CollectResources
+    {
+        public static void Main()
+        {
+            List<string> inputResourses = Console.ReadLine().Trim().Split().ToList();
+            var validItems = new List<string> { "stone", "gold", "wood", "food" };
+            int n = int.Parse(Console.ReadLine());
+            List<ulong> results = new List<ulong>();
+
+            for (int i = 0; i < n; i++)
+            {
+                List<int> collectedIndices = new List<int>();
+                string[] commands = Console.ReadLine().Trim().Split();
+                int start = int.Parse(commands[0]);
+                int step = int.Parse(commands[1]);
+                int currentIndex = start;
+                ulong currentMax = 0;
+
+                bool hasValid = false;
+
+                foreach (var resourse in inputResourses)
+                {
+                    var splittedRes = resourse.Split('_');
+
+                    if (validItems.Contains(splittedRes[0]))
+                    {
+                        hasValid = true;
+                    }
+                }
+
+                while (true)
+                {
+                    var currentResource = inputResourses[currentIndex];
+
+                    var splittedRes = currentResource.Split(new[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (splittedRes.Length == 2 && hasValid)
+                    {
+                        var currName = splittedRes[0];
+                        var currQuantity = ulong.Parse(splittedRes[1]);
+
+                        if (validItems.Contains(currName))
+                        {
+                            currentMax += currQuantity;
+                            collectedIndices.Add(currentIndex);
+                        }
+
+                        currentIndex = (currentIndex + step) % inputResourses.Count;
+
+                        if (collectedIndices.Contains(currentIndex))
+                        {
+                            break;
+                        }
+                    }
+                    else if (splittedRes.Length == 1)
+                    {
+                        var currName = splittedRes[0];
+
+                        if (validItems.Contains(currName))
+                        {
+                            currentMax += 1;
+                            collectedIndices.Add(currentIndex);
+                        }
+
+                        currentIndex = (currentIndex + step) % inputResourses.Count;
+
+                        if (collectedIndices.Contains(currentIndex))
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                results.Add(currentMax);
+            }
+
+            Console.WriteLine(results.Max());
+        }
+    }
+}
---
+++ 01.Advanced-CSharp/00.Exam/02.ParkingSystem/ParkingSystem.cs
@@ -0,0 +1,91 @@
+﻿namespace Problem2
+{
+    using System;
+
+    public class ParkingSystem
+    {
+        public static void Main()
+        {
+            var dimensions = Console.ReadLine().Split();
+            int rows = int.Parse(dimensions[0]);
+            int cols = int.Parse(dimensions[1]);
+            int[,] parkingLot = new int[rows, cols];
+
+            var details = Console.ReadLine();
+
+            while (details != "stop")
+            {
+                var splitted = details.Split();
+                int entryRow = int.Parse(splitted[0]);
+
+                int desireRow = int.Parse(splitted[1]);
+                int desireCol = int.Parse(splitted[2]);
+
+
+                int stepsCounter = 1 + Math.Abs(desireRow - entryRow);
+
+                if (parkingLot[desireRow, desireCol] == 0)
+                {
+                    FreeSpot(parkingLot, desireRow, desireCol, stepsCounter);
+                }
+                else
+                {
+                    bool isParked = false;
+                    var freeCol = FindFreeSpot(desireCol, parkingLot, desireRow, ref isParked);
+
+                    PrintResult(isParked, stepsCounter, freeCol, desireRow);
+                }
+
+                details = Console.ReadLine();
+            }
+        }
+
+        private static int FindFreeSpot(int desireCol, int[,] parkingLot, int desireRow, ref bool isParked)
+        {
+            int freeCol = 0;
+
+            for (int i = Math.Max(desireCol - 1, 1); i >= 1; i--)
+            {
+                if (desireCol - 1 >= 1 && parkingLot[desireRow, i] == 0 && isParked == false)
+                {
+                    parkingLot[desireRow, i] = 1;
+                    freeCol = i;
+                    isParked = true;
+                    break;
+                }
+            }
+
+            for (int i = Math.Min(desireCol + 1, parkingLot.GetLength(1) - 1); i < parkingLot.GetLength(1); i++)
+            {
+                if (desireCol + 1 < parkingLot.GetLength(1) && parkingLot[desireRow, i] == 0 && isParked == false)
+                {
+                    parkingLot[desireRow, i] = 1;
+                    freeCol = i;
+                    isParked = true;
+                    break;
+                }
+            }
+            return freeCol;
+        }
+
+        private static void FreeSpot(int[,] parkingLot, int desireRow, int desireCol, int stepsCounter)
+        {
+            parkingLot[desireRow, desireCol] = 1;
+            stepsCounter += desireCol;
+            Console.WriteLine(stepsCounter);
+        }
+
+        private static void PrintResult(bool isParked, int stepsCounter, int availableColumn, int desireRow)
+        {
+            if (isParked)
+            {
+                stepsCounter += availableColumn;
+                Console.WriteLine(stepsCounter);
+            }
+            else
+            {
+                Console.WriteLine("Row {0} full", desireRow);
+            }
+        }
+    }
+}
